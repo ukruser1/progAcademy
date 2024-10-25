1e7359562fde668b7796123f0985744ff4746a53
ukruser1 <new.spamm@gmail.com>
2024-10-25 16:38:43 +0800

    Second task

--- Main.java
+++ Main.java
@@ -1,15 +1,24 @@
-package example.org;
+package org.example;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
-        System.out.printf("How are you?");
+        System.out.println("Please enter how much fuel does your car consume.");
 
         var scanner = new Scanner(System.in);
-        var answer = scanner.nextLine();
+        var fuelConsumption = scanner.nextDouble();
 
-        System.out.printf("User answered: " + answer);
+        while(true) {
+            System.out.println("Please enter how many kilometers did your car ride.");
+            var distance = scanner.nextDouble();
+
+            if(distance == 0)
+            {
+                break;
+            }
+            var result = distance/100*fuelConsumption;
+            System.out.println("Your car consumed: " + result);
         }
     }
+}
